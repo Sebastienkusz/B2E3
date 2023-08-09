c8ff79b1d1658c452c503a6c7ae4173774ad7dde
Sebastienkusz <kusz.sebastien@gmail.com>
2023-08-09 16:56:44 +0200

    feat: peering in one module

--- Terraform/modules/network/main.tf
+++ Terraform/modules/network/main.tf
@@ -1,15 +1,44 @@
-resource "azurerm_virtual_network" "main" {
-  name                = "vnet-${lower(var.location)}"
-  location            = var.location
+resource "azurerm_virtual_network" "main1" {
+  name                = "${var.resource_group}-vnet-${lower(var.location_1)}"
+  location            = var.location_1
   resource_group_name = var.resource_group
-  address_space       = var.vnet_cidr
-  tags                = var.tags
+  address_space       = var.vnet_1
 }
 
-resource "azurerm_subnet" "main" {
-  for_each             = toset(var.subnet_cidrs)
-  name                 = "subnet-${index(var.subnet_cidrs, each.key)}"
-  virtual_network_name = azurerm_virtual_network.main.name
+resource "azurerm_virtual_network" "main2" {
+  name                = "${var.resource_group}-vnet-${lower(var.location_2)}"
+  location            = var.location_2
+  resource_group_name = var.resource_group
+  address_space       = var.vnet_2
+}
+
+resource "azurerm_subnet" "main1" {
+  for_each             = toset(var.subnet_1)
+  name                 = "${var.resource_group}-subnet-${index(var.subnet_1, each.key)}"
+  virtual_network_name = azurerm_virtual_network.main1.name
   resource_group_name  = var.resource_group
   address_prefixes     = [each.value]
+}
+
+resource "azurerm_subnet" "main2" {
+  for_each             = toset(var.subnet_2)
+  name                 = "${var.resource_group}-subnet-${index(var.subnet_2, each.key)}"
+  virtual_network_name = azurerm_virtual_network.main2.name
+  resource_group_name  = var.resource_group
+  address_prefixes     = [each.value]
+}
+
+# Peering subnets
+resource "azurerm_virtual_network_peering" "vnet-1" {
+  name                      = "${var.resource_group}-peer1to2"
+  resource_group_name       = var.resource_group
+  virtual_network_name      = azurerm_virtual_network.main1.name
+  remote_virtual_network_id = azurerm_virtual_network.main2.id
+}
+
+resource "azurerm_virtual_network_peering" "vnet-2" {
+  name                      = "${var.resource_group}-peer2to1"
+  resource_group_name       = var.resource_group
+  virtual_network_name      = azurerm_virtual_network.main2.name
+  remote_virtual_network_id = azurerm_virtual_network.main1.id
 }
