db2f3952905be0f9eef50e7426f0ca5121a23762
Sebastienkusz <kusz.sebastien@gmail.com>
2023-08-23 21:22:13 +0200

    feat: output for users

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -3,6 +3,14 @@ output "pass" {
   sensitive = true
 }
 
-output "ssh_redis" {
+output "admin_ssh_redis" {
   value = "ssh -i ${local_file.admin_rsa_file.filename} ${local.admin_username}@${module.vm.vm_fqdn}"
+}
+
+output "list_users_ssh_redis" {
+  value = [ for user_name, user_value in local.users :user_name ]
+}
+
+output "users_ssh_redis" {
+  value = "ssh username@${module.vm.vm_fqdn}"
 }
