637d5e1608469b383da3da8e64ba46f5fd9927a7
Sebastienkusz <kusz.sebastien@gmail.com>
2023-08-28 19:45:59 +0200

    feat: roles users ansible

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "b2e1-gr2"
-    storage_account_name = "b2e1gr22i2tsn"
-    container_name       = "2i2tsn"
+    storage_account_name = "b2e1gr2tfstate"
+    container_name       = "tfstate"
     key                  = "b2e1-gr2/terraform.tfstate"
   }
 }
