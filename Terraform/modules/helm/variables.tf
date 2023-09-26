97b44df3136ced47fdcab7bbc383aa4234c52f42
Sebastienkusz <kusz.sebastien@gmail.com>
2023-09-26 16:42:07 +0200

    feat: update descriptions in helm module

--- Terraform/modules/helm/variables.tf
+++ Terraform/modules/helm/variables.tf
@@ -1,12 +1,12 @@
 variable "resource_group_name" {
   type        = string
-  description = "(Required) The name of the resource group in which to create the virtual network."
+  description = "(Required) The name of the resource group."
   nullable    = false
 }
 
 variable "subscription_id" {
   type        = string
-  description = "(Required) The name of the resource group in which to create the virtual network."
+  description = "(Required) The subscription id."
   nullable    = false
 }
 
@@ -14,94 +14,94 @@ variable "subscription_id" {
 # Prometheus
 variable "prometheus_chart" {
   type        = string
-  description = "(Required) The name of the resource group in which to create the virtual network."
+  description = "(Required) The chart of prometheus."
   nullable    = false
 }
 
 variable "prometheus_name" {
   type        = string
-  description = "(Required) The name of the resource group in which to create the virtual network."
+  description = "(Required) The name of prometheus in resource helm."
   nullable    = false
 }
 
 variable "prometheus_namespace_creation" {
   type        = bool
-  description = "(Required) The name of the resource group in which to create the virtual network."
+  description = "(Required) Creation of the namaspace for prometheus."
   nullable    = false
   default     = true
 }
 
 variable "prometheus_namespace" {
   type        = string
-  description = "(Required) The name of the resource group in which to create the virtual network."
+  description = "(Required) The namespace of prometheus in resource helm."
   nullable    = false
 }
 
 variable "prometheus_repository" {
   type        = string
-  description = "(Required) The name of the resource group in which to create the virtual network."
+  description = "(Required) The repository of prometheus in resource helm."
   nullable    = false
 }
 # ---------------------------------------------------------------------------------
 # Grafana
 variable "grafana_chart" {
   type        = string
-  description = "(Required) The name of the resource group in which to create the virtual network."
+  description = "(Required) The chart of grafana."
   nullable    = false
 }
 
 variable "grafana_name" {
   type        = string
-  description = "(Required) The name of the resource group in which to create the virtual network."
+  description = "(Required) The name of grafana in resource helm."
   nullable    = false
 }
 
 variable "grafana_namespace" {
   type        = string
-  description = "(Required) The name of the resource group in which to create the virtual network."
+  description = "(Required) The namespace of grafana in resource helm."
   nullable    = false
 }
 
 variable "grafana_repository" {
   type        = string
-  description = "(Required) The name of the resource group in which to create the virtual network."
+  description = "(Required) The repository of grafana in resource helm."
   nullable    = false
 }
 # ---------------------------------------------------------------------------------
 # Ingress
 variable "ingress_chart" {
   type        = string
-  description = "(Required) The name of the resource group in which to create the virtual network."
+  description = "(Required) The chart of ingress."
   nullable    = false
 }
 
 variable "ingress_name" {
   type        = string
-  description = "(Required) The name of the resource group in which to create the virtual network."
+  description = "(Required) The name of ingress in resource helm."
   nullable    = false
 }
 
 variable "ingress_namespace_creation" {
   type        = bool
-  description = "(Required) The name of the resource group in which to create the virtual network."
+  description = "(Required) Creation of the namaspace for ingress."
   nullable    = false
   default     = true
 }
 
 variable "ingress_namespace" {
   type        = string
-  description = "(Required) The name of the resource group in which to create the virtual network."
+  description = "(Required) The namespace of ingress in resource helm."
   nullable    = false
 }
 
 variable "ingress_repository" {
   type        = string
-  description = "(Required) The name of the resource group in which to create the virtual network."
+  description = "(Required) The repository of ingress in resource helm."
   nullable    = false
 }
 
 variable "gateway_name" {
   type        = string
-  description = "(Required) The name of the resource group in which to create the virtual network."
+  description = "(Required) The name of the gateway."
   nullable    = false
 }
